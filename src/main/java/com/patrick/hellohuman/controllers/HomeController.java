58738c9ebbca6e906d0e6fce497acbe8fd867a7d
Patrick Kim <patrick.kim408@gmail.com>
2017-08-07 15:59:45 -0800

    initial commit

---
+++ src/main/java/com/patrick/hellohuman/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.patrick.hellohuman.controllers;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String index() {
+		return "index.jsp";
+	}
+
+}
